0a015603670d00bb51bc8ffbeae548e890fa927e
linzetong <1049054285@qq.com>
2018-05-26 18:25:06 +0800

    test logger2

--- app01/views.py
+++ app01/views.py
@@ -33,7 +33,6 @@ def ali():
 
 def page1(request):
     if request.method == "GET":
-        logger.info(request.get_host())
         return render(request, 'page1.html')
     else:
         money = float(request.POST.get('money'))
@@ -72,7 +71,7 @@ def page2(request):
         for k, v in post_data.items():
             post_dict[k] = v[0]
 
-        logger.info('post_dict', post_dict)
+        logger.info('post_dict'+str(post_dict))
         # print(post_dict)
         """
         {'gmt_create': '2017-11-24 14:53:41', 'charset': 'utf-8', 'gmt_payment': '2017-11-24 14:53:48', 'notify_time': '2017-11-24 14:57:05', 'subject': '充气式韩红', 'sign': 'YwkPI9BObXZyhq4LM8//MixPdsVDcZu4BGPjB0qnq2zQj0SutGVU0guneuONfBoTsj4XUMRlQsPTHvETerjvrudGdsFoA9ZxIp/FsZDNgqn9i20IPaNTXOtQGhy5QUetMO11Lo10lnK15VYhraHkQTohho2R4q2U6xR/N4SB1OovKlUQ5arbiknUxR+3cXmRi090db9aWSq4+wLuqhpVOhnDTY83yKD9Ky8KDC9dQDgh4p0Ut6c+PpD2sbabooJBrDnOHqmE02TIRiipULVrRcAAtB72NBgVBebd4VTtxSZTxGvlnS/VCRbpN8lSr5p1Ou72I2nFhfrCuqmGRILwqw==', 'buyer_id': '2088102174924590', 'invoice_amount': '666.00', 'version': '1.0', 'notify_id': '11aab5323df78d1b3dba3e5aaf7636dkjy', 'fund_bill_list': '[{"amount":"666.00","fundChannel":"ALIPAYACCOUNT"}]', 'notify_type': 'trade_status_sync', 'out_trade_no': 'x21511506412.4733646', 'total_amount': '666.00', 'trade_status': 'TRADE_SUCCESS', 'trade_no': '2017112421001004590200343962', 'auth_app_id': '2016082500309412', 'receipt_amount': '666.00', 'point_amount': '0.00', 'app_id': '2016082500309412', 'buyer_pay_amount': '666.00', 'sign_type': 'RSA2', 'seller_id': '2088102172939262'}
@@ -81,11 +80,11 @@ def page2(request):
         sign = post_dict.pop('sign', None)
 
         status = alipay.verify(post_dict, sign)
-        logger.info('POST验证', status)
+        logger.info('POST验证:'+str(status))
         # print('POST验证', status)
         if status:
-            logger.info("post_dict['stade_status']: ", post_dict['stade_status'])
-            logger.info("post_dict['out_trade_no']: ", post_dict['out_trade_no'])
+            logger.info("post_dict['stade_status']: "+str(post_dict['stade_status']))
+            logger.info("post_dict['out_trade_no']: "+str(post_dict['out_trade_no']))
             # print("post_dict['stade_status']: ", post_dict['stade_status'])
             # print("post_dict['out_trade_no']: ", post_dict['out_trade_no'])
 
@@ -95,6 +94,6 @@ def page2(request):
         params = request.GET.dict()
         sign = params.pop('sign', None)
         status = alipay.verify(params, sign)
-        logger.info('GET验证', status)
+        logger.info('GET验证:' + str(status))
         # print('GET验证', status)
         return HttpResponse('支付成功')
